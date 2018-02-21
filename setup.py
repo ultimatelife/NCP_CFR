9ed95980b3aa8847f2011a193fec4f89928d01ca
Kunwoo Kim <geonwoo.kim@navercorp.com>
2018-02-22 00:38:59 +0900

    config commit

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(name='NCP_CFR',
       description="""This is for CFR(Clova Face Recognition) service of NAVER Cloud Platform\nhttps://github.com/ultimatelife/NCP_CFR""",
-      version='0.012',
+      version='0.14',
       url='https://github.com/ultimatelife/NCP_CFR.git',
       author='geonwoo.kim',
+      keywords=['NCP', 'CFR', 'Clova', 'clova'],
       author_email='drama0708@gmail.com',
       license='Naver Cloud Platform',
+      python_requires='>=3.6',
       classifiers=[
           'Programming Language :: Python :: 3.6'
       ],
